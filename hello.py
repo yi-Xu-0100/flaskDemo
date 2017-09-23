ad636bf88ecada3faf038a3ffad23dfb7cb171b8
yi-xu-0100 <xjy123456120@163.com>
2017-09-23 12:02:45 +0800

    add form

--- hello.py
+++ hello.py
@@ -7,12 +7,48 @@ description:
 
 __author__ = 'yi_Xu'
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask,render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from datetime import datetime
 
-@app.route('/')
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'wwwyixujucn'
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+manager = Manager(app)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return '<h1>Hello world!</h1>'
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    manager.run()
